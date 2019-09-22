670fe17036cd01b927ef40b1b86366f3b9260731
huangjian <1342042894@qq.com>
2019-09-22 12:33:32 +0800

    add many code

---
+++ build.sh
@@ -0,0 +1,9 @@
+rm hello_world
+rm prompt
+rm doge_grammer
+cc -std=c99 -Wall hello_world.c -o hello_world
+cc -std=c99 -Wall prompt.c -ledit -o prompt
+cc -std=c99 -Wall doge_grammer.c mpc.c -ledit -lm -o doge_grammer
+cc -g -std=c99 -Wall parsing.c mpc.c -ledit -lm -o parsing
+cc -std=c99 -Wall parsing_ab.c mpc.c -ledit -lm -o parsing_ab
+cc -std=c99 -Wall parsing_ab2.c mpc.c -ledit -lm -o parsing_ab2
---
+++ clean.sh
@@ -0,0 +1,6 @@
+rm hello_world
+rm prompt
+rm doge_grammer
+rm parsing
+rm parsing_ab
+rm parsing_ab2
